36ae54dde55acdd7cb0874a66c8ce29b835b104b
Matt Benson <matthewbenson17@augustana.edu>
2018-09-24 12:24:34 -0500

    initialization of the Video class

---
+++ ProjectIbis/src/edu/augustana/csc285/Ibis/Video.java
@@ -0,0 +1,42 @@
+package edu.augustana.csc285.Ibis;
+
+import java.io.File;
+import org.opencv.core.*;
+
+import org.opencv.videoio.VideoCapture;
+
+import javafx.scene.shape.Rectangle;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
+public class Video {
+
+	private VideoCapture vidCap = new VideoCapture();
+	private FileChooser fileChooser = new FileChooser();
+	public Window mainWindow;
+	
+	public File videoFile;
+	private String videoPath;
+	
+	private double frameRate;
+	private double xPixelsPerCm;
+	private double yPixelsPerCm;
+	private int totalNumFrames;
+	private int startFrameNum;
+	private int endFrameNum;
+	private Rectangle arenaBounds;
+	
+	
+	public File getVideoFile() {
+		return this.videoFile;
+	}
+	
+	public void importVideo() {
+		this.videoFile = fileChooser.showOpenDialog(mainWindow);
+		this.videoPath = videoFile.getAbsolutePath();
+		
+	}
+	
+}
+
+
