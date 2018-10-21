e8f8f20a5d1b2d6aed2762a34a57fd7268ae0121
Kevin <kevinmonroy17@augustana.edu>
2018-10-21 12:30:28 -0500

    substantianl progress in calibration window and started to dynamically create radio buttons

--- ProjectIbis/src/edu/augustana/csc285/Ibis/CalibrationWindowController.java
+++ ProjectIbis/src/edu/augustana/csc285/Ibis/CalibrationWindowController.java
@@ -2,6 +2,8 @@ package edu.augustana.csc285.Ibis;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import edu.augustana.csc285.Ibis.datamodel.ProjectData;
@@ -18,6 +20,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -40,6 +43,11 @@ public class CalibrationWindowController {
 	private Button removeButton;
 	@FXML
 	private Button finishButton;
+	@FXML
+	private TextField numberOfChicksLabel;
+	private ArrayList<String> names = new ArrayList<String>();
+	
+	private int numberOfChicks = 0;
 	
 	private ProjectData project;
 	
@@ -57,20 +65,25 @@ public class CalibrationWindowController {
 	
 	@FXML
 	public void handleFinishButton() throws IOException {
-		
-		System.out.println("handle Finish!");
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
-		AnchorPane root = (AnchorPane) loader.load();
-		
-		MainWindowController nextController = loader.getController();
-		nextController.setProject(project);
-		
-		Scene nextScene = new Scene(root, root.getPrefWidth(), root.getPrefHeight());
-		nextScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		Stage primary = (Stage) finishButton.getScene().getWindow();
-		primary.setScene(nextScene);
-		primary.setTitle("Chick Tracker 1.0");		
+		System.out.println("names" + names.size());
+		if (numberOfChicks>0) {
+
+			System.out.println("handle Finish!");
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+			AnchorPane root = (AnchorPane) loader.load();
+			
+			MainWindowController nextController = loader.getController();
+			nextController.setProject(project);
+//			nextController.animalTrackModifier(numberOfChicks, names); //this is breaking the code, someone fix it
+			
+			Scene nextScene = new Scene(root, root.getPrefWidth(), root.getPrefHeight());
+			nextScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Stage primary = (Stage) finishButton.getScene().getWindow();
+			primary.setScene(nextScene);
+			primary.setTitle("Chick Tracker 1.0");					
+		}
 	}
+	
 	public void setVideo(String filePath) throws FileNotFoundException {
 		project = new ProjectData(filePath);
 		project.getVideo().setXPixelsPerCm(6);
@@ -106,12 +119,20 @@ public class CalibrationWindowController {
 		// Traditional way to get the response value.
 		Optional<String> result = dialog.showAndWait();
 		if (result.isPresent()){
-		    System.out.println("Your name: " + result.get());
+			numberOfChicks++;
+			numberOfChicksLabel.setText("" + numberOfChicks);
 		}
+		names.add(result.get());
+		System.out.println(names.size());
 	}
 	
 	@FXML
 	public void handleRemoveButton() {
-		
+		if (numberOfChicks>0) {
+			numberOfChicks--;
+			numberOfChicksLabel.setText("" + numberOfChicks);
+			names.remove(names.size()-1);
+			System.out.println(names.size());
+		}
 	}
 }
